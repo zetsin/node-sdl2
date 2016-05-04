36ab50b0420d4c233af22a9430990c970cd2a594
Rob Loach <robloach@gmail.com>
2016-05-04 03:32:59 -0400

    Update to node-sdl2-prebuilt and add SDL_timer

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-var path = require('path');
-module.exports = function (name) {
-	return require(path.join(__dirname, 'lib', name));
+var extend = require('extend-shallow');
+var fs = require('fs');
+
+// Retrieve the list of available modules.
+var modules = [
+  'SDL',
+  'SDL_blendmode',
+  'SDL_keycode',
+  'SDL_rwops',
+  'SDL_syswm',
+  'SDL_error',
+  'SDL_joystick',
+  'SDL_pixels',
+  'SDL_scancode',
+  'SDL_touch',
+  'SDL_events',
+  'SDL_rect',
+  'SDL_stdinc',
+  'SDL_version.js',
+  'SDL_gesture',
+  'SDL_keyboard',
+  'SDL_render',
+  'SDL_surface',
+  'SDL_timer',
+  'SDL_video'
+];
+
+// Merge all the modules together.
+var sdl = {};
+for (var i in modules) {
+	extend(sdl, require('./lib/' + modules[i]));
 }
+
+module.exports = sdl;
