d9c6bb84e70a661289dd29af9898ae1797cdf3fc
Rob Loach <robloach@gmail.com>
2016-05-15 19:45:25 -0500

    Use fs.readdirSync to bring in modules

--- index.js
+++ index.js
@@ -2,28 +2,7 @@ var extend = require('extend-shallow');
 var fs = require('fs');
 
 // Retrieve the list of available modules.
-var modules = [
-  'SDL',
-  'SDL_blendmode',
-  'SDL_keycode',
-  'SDL_rwops',
-  'SDL_syswm',
-  'SDL_error',
-  'SDL_joystick',
-  'SDL_pixels',
-  'SDL_scancode',
-  'SDL_touch',
-  'SDL_events',
-  'SDL_rect',
-  'SDL_stdinc',
-  'SDL_version.js',
-  'SDL_gesture',
-  'SDL_keyboard',
-  'SDL_render',
-  'SDL_surface',
-  'SDL_timer',
-  'SDL_video'
-];
+var modules = fs.readdirSync(__dirname + '/lib/');
 
 // Merge all the modules together.
 var sdl = {};
